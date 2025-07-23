c5a8672c037683f074b7f495db24ad9293aa9db6
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-23 20:11:21 +0200

    Add menu state management and integrate Menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import data from "./data";
+import Menu from "./components/Menu";
 import Title from "./components/Title";
+
 function App() {
+  const [menu, setMenu] = useState(data);
+
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
+        <Menu menu={menu} />
       </section>
     </main>
   );
