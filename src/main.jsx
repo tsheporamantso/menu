72bc6dc2e89399022e410a247238528172595ee3
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-24 08:09:30 +0200

    Implement routing with react-router-dom and set up HomePage and App components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import HomePage from "./Pages/HomePage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <div>404 page not found</div>,
+  },
+  {
+    path: "/App",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
